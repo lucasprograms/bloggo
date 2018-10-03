220119d4ba02f917897bd5355c82810969d0f37f
lucas <lucasprograms@gmail.com>
2018-10-03 19:11:00 -0400

    init + some content

---
+++ front-page.php
@@ -0,0 +1,6 @@
+<?php get_header() ?>
+  <div class="static-page__front text-center col-12 jumbotron mt-1">
+    <h2>Welcome to <span class="title-font"><?php bloginfo('name') ?></span>!</h2>
+    <h6>You have unearthed the bloggiest bloggo in the blogosphere</h6>
+  </div>
+<?php get_footer() ?>
---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- bootstrap css -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <title>Bloggo</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
+</head>
+<body class="container">
+  <header class="row border-bottom">
+    <div class="row col-4 site-title-container">
+      <h1 class="col-12 site-title title-font"><?php bloginfo('name') ?></h1>
+      <h5 class="col-12 site-description"><?php bloginfo('description') ?></h5>
+    </div>
+  </header>
+  
