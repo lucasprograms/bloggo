8845ff7a10181673b710fe63c75d6acf934431c1
lucas <lucasprograms@gmail.com>
2018-10-07 20:41:46 -0400

    tweak head + foot

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <title><?php bloginfo('name') ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
+  <?php wp_head() ?>
 </head>
 <body class="container">
   <header class="rounded-bottom row mb-5 pt-4 pb-4">
